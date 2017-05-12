bda5e877f970c3b50dd26cf6e84fd8519d4382e0
Dane Johnson <dane_johnson@ymail.com>
2017-05-12 12:59:35 -0500

    added installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+myname=$(basename "$0")
+
+for f in $( ls -a | awk '!/^(.|..|.git|'"$myname"')$/' ); do
+    dest="$HOME"/"$f"
+    rm -rf "$dest"
+    if [ -d "$f" ]; then
+	mkdir "$dest"
+	lndir -silent $(pwd)/"$f" "$dest" #Need pwd because lndir expects fromdir relative to todir
+    else
+	ln -s "$f" "$HOME"
+    fi
+done
+exit
+	      
+	    
