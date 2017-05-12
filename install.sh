cda23934b59b9727932e5e5bb17c2983dead26ad
Dane Johnson <dane_johnson@ymail.com>
2017-05-12 16:46:37 -0500

    use ln instead of lndir for portability

--- install.sh
+++ install.sh
@@ -6,12 +6,7 @@ myname=$(basename "$0")
 for f in $( ls -a | awk '!/^(.|..|.git|'"$myname"')$/' ); do
     dest="$HOME"/"$f"
     rm -rf "$dest"
-    if [ -d "$f" ]; then
-	mkdir "$dest"
-	lndir -silent $(pwd)/"$f" "$dest" #Need pwd because lndir expects fromdir relative to todir
-    else
-	ln -s $(pwd)/"$f" "$HOME"
-    fi
+    ln -s $(pwd)/"$f" "$HOME"
 done
 exit
 	      
