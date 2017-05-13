b21759cabc1f897d1f0e10694ca4051e8696e584
Dane Johnson <dane_johnson@ymail.com>
2017-05-12 20:36:14 -0500

    More blacklist

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@
 cd $(dirname "$0")
 myname=$(basename "$0")
 
-for f in $( ls -a | awk '!/^(.|..|.git|'"$myname"')$/' ); do
+for f in $( ls -a | awk '!/^(.|..|.git|.gitignore|.gitmodules|'"$myname"')$/' ); do
     dest="$HOME"/"$f"
     rm -rf "$dest"
     ln -s $(pwd)/"$f" "$HOME"
 done
 exit
-	      
-	    
