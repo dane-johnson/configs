5f341377d69aa05ed7de9bf038ad2d34939cd502
Dane Johnson <dane_johnson@ymail.com>
2017-05-27 18:48:04 -0500

    Changed https to http

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
 ;;Install MELPA
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "http://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
