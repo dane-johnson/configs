cbd86117cae76597bba2ee4da87249a597c3a46e
dane-johnson <dane_johnson@ymail.com>
2017-07-03 17:25:15 -0500

    zsh portiblility

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -64,3 +64,18 @@
 (load "setup-elisp.el")
 (load "setup-python.el")
 (load "setup-javascript.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (clojure-mode-extra-font-locking cider paredit rainbow-delimiters atom-one-dark-theme yasnippet auto-complete yaml-mode ace-jump-mode)))
+ '(standard-indent 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
