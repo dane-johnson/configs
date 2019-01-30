ced083e67f657be5f328fee1778866b1eb11c798
Dane Johnson <gkulg@monsanto.com>
2019-01-30 15:10:41 -0600

    Fixed regexp for setup-javascript

--- .emacs.d/customizations/setup-javascript.el
+++ .emacs.d/customizations/setup-javascript.el
@@ -1,4 +1,4 @@
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\(x\\|on\\)?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (setq web-mode-indent-style 2
       web-mode-code-indent-offset 2)
