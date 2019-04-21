53b7b9e38b60326775374b64c6c148a9b0c5ef76
Dane Johnson <dane_johnson@ymail.com>
2019-04-20 19:48:28 -0500

    Added jump to assignment and jump to definitions keybindings to elpy mode

--- .emacs.d/customizations/setup-python.el
+++ .emacs.d/customizations/setup-python.el
@@ -1,2 +1,4 @@
 (add-hook 'python-mode-hook #'rainbow-delimiters-mode)
 (setq python-indent-offset 2)
+(define-key elpy-mode-map (kbd "C-c d") 'elpy-goto-definition)
+(define-key elpy-mode-map (kbd "C-c a") 'elpy-goto-assignment)
